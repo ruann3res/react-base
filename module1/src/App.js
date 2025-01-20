000d54c5484ea97994001d5e1b329f112b1deac7
ruann3res <ruanpablo1218@gmail.com>
2025-01-20 15:42:42 -0300

    feat: learning about lists && component lists && key prop

--- module1/src/App.js
+++ module1/src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import Post from "./Post";
 import Header from "./Header";
 
+const posts = [
+    { title: "Post #01", subtittle: "Sub #01", likes: 20, },
+    { title: "Post #02", subtittle: "Sub #02", likes: 10 },
+    { title: "Post #03", subtittle: "Sub #03", likes: 40 }
+
+
+]
+
 export default function App() {
     return (
         <>
@@ -9,34 +17,16 @@ export default function App() {
                 <h2>Meu primeiro projeto</h2>
             </Header>
             <hr />
-            <Post
-                post={{
-                    title: "Olá Mundo!",
-                    subtittle: "Esse e meu site!"
-                }}
-                likes={20}
-            />
-            <Post
-                post={{
-                    title: "Olá Mundo 1",
-                    subtittle: "Esse e meu site 1"
-                }}
-                likes={10}
-            />
-            <Post
-                post={{
-                    title: "Olá Mundo 2",
-                    subtittle: "Esse e meu site 2"
-                }}
-                likes={24}
-            />
-            <Post
-                post={{
-                    title: "Olá Mundo 1",
-                    subtittle: "Esse e meu site 1"
-                }}
-                likes={4}
-            />
+            {posts.map(post => (
+                <Post
+                    key={post.title}
+                    likes={post.likes}
+                    post={{
+                        title: post.title,
+                        subtittle: post.subtittle
+                    }}
+                />
+            ))}
 
         </>
     )
