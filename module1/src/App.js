cb918789fd201dfd53d315fb79dc43447b61250c
ruann3res <ruanpablo1218@gmail.com>
2025-01-20 14:35:19 -0300

    feat: learning about childrens

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Post from "./Post";
+import Header from "./Header";
 
 export default function App() {
     return (
         <>
-            <h1>Componente App</h1>
-            <h2>Meu primeiro projeto</h2>
+            <Header>
+                <h2>Meu primeiro projeto</h2>
+            </Header>
             <hr />
             <Post title="Olá Mundo" subtittle="Esse e meu site" />
             <Post title="Olá Mundo 1" subtittle="Esse e meu site 1" />
