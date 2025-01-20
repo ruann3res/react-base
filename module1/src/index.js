98783d8c934340db2de8306898ee17631c84d5ae
ruann3res <ruanpablo1218@gmail.com>
2025-01-20 11:33:26 -0300

    chore: module 1, config babel && webpack

---
+++ module1/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1 id="testeReact">
+        <span>
+            Olá Mundo!!
+        </span>
+    </h1>,
+    document.getElementById('root')
+)
---
+++ module1/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle[hash].js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
