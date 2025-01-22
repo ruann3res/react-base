0918a7803b0365925ef383135e5fd0fabf5eff93
ruann3res <ruanpablo1218@gmail.com>
2025-01-22 14:06:05 -0300

    feat: learning about react states

--- module1/src/App.js
+++ module1/src/App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-    { title: "Post #01", subtittle: "Sub #01", likes: 20, },
-    { title: "Post #02", subtittle: "Sub #02", likes: 10 },
-    { title: "Post #03", subtittle: "Sub #03", likes: 40 }
-
-
-]
-
 export default function App() {
+    const [posts, setPosts] = useState([
+        { id: Math.random(), title: "Post #01", subtittle: "Sub #01", likes: 20, },
+        { id: Math.random(), title: "Post #02", subtittle: "Sub #02", likes: 10 },
+        { id: Math.random(), title: "Post #03", subtittle: "Sub #03", likes: 40 }
+    ])
+
+    function handleUpdatePosts() {
+        setPosts((prevState) => [
+            ...prevState,
+            {
+                id: Math.random(),
+                title: `Post #${prevState.length}`,
+                subtittle: `Sub #${prevState.length}`,
+                likes: 40
+            }])
+    }
     return (
         <>
             <Header title="Primeiro Projeto React">
                 <h2>Meu primeiro projeto</h2>
             </Header>
+            <br />
+            <button onClick={handleUpdatePosts}>
+                Atualizar Posts
+            </button>
             <hr />
             {posts.map(post => (
                 <Post
@@ -27,7 +39,10 @@ export default function App() {
                     }}
                 />
             ))}
-
         </>
     )
 }
+
+
+
+
