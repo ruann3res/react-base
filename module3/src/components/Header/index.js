acf16c00afe75993d071c7bf17fe3721e394b90c
ruann3res <ruanpablo1218@gmail.com>
2025-02-04 14:29:39 -0300

    feat: learning about theming in styled components

---
+++ module3/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Ruan's Blog</h1>
+      <button type="button">🌞</button>
+    </Container>
+  );
+}
